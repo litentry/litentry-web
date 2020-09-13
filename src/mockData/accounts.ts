1608f0e54bdb4c636e62bde31c161ae76d36ba96
Hanwen Cheng <heawen.cheng@gmail.com>
2020-09-14 00:30:09 +0200

    update new identity address with new network

--- src/mockData/accounts.ts
+++ src/mockData/accounts.ts
@@ -1,4 +1,4 @@
 export const musicAccountSeedPhrase = 'pepper immense size wrap unlock toe lobster wheel exotic prize lawn vibrant';
 export const musicAccountAddress = '15QRHkvZfk57TYkcrWJ8bSSCpJ77rMQ7BeKr1R6g7Mx8EpHM';
-export const musicAccountIdentity = '0xfd67478ef48ddd1c986e9d95fbde1bbb7eef45b8c6712489f78dae64cc20c259'
+export const musicAccountIdentity = '0x14b5cfbbf79b54a197a02d311723715bd78fba5a697edd9e4e8569b0958bc1c9'
 
